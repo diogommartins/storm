e92cff301c2c04d955fa5fc121e67a72cb832929
diogomartins <diogo.martins@uniriotec.br>
2015-07-01 19:48:14 -0300

    Initial Commit

---
+++ storm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  storm
+//
+//  Created by Diogo Magalhães Martins on 7/1/15.
+//  Copyright (c) 2015 Diogo Magalhães Martins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ storm/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  storm
+//
+//  Created by Diogo Magalhães Martins on 7/1/15.
+//  Copyright (c) 2015 Diogo Magalhães Martins. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
